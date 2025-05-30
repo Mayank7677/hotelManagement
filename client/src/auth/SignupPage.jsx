f7f500c344f729ed1d0693368d971d690a2c0b0b
Mayank Panwar <mayankpanwar066@gmail.com>
2025-05-30 12:33:33 +0530

    f

--- client/src/auth/SignupPage.jsx
+++ client/src/auth/SignupPage.jsx
@@ -68,7 +68,7 @@ const SignupPage = () => {
       navigate("/");
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data.message);
+      toast.error(error?.response?.data?.message);
     }
   };
 
