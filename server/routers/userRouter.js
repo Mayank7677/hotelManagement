50c76ae939cc76964377d206fcbc61fc5ca21f99
Mayank Panwar <mayankpanwar066@gmail.com>
2025-06-19 18:08:17 +0530

    logout fix

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -10,7 +10,7 @@ router.post("/resetPass", userController.resetPassword);
 router.post("/newPass", userController.newPassword);
 router.put("/updateProfile", userController.editProfile);
 router.get("/getProfile", auth, userController.getUser);
-router.get("/logout", auth, userController.logout);
+router.get("/logout", userController.logout);
 
 
 module.exports = router;
